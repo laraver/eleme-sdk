21236e6fa224727affc727d5b37c829b797edc64
JokerLinly <JokerLinly@gmail.com>
2017-05-31 15:21:31 +0800

    fixed name

---
+++ src/Eleme/Packs/Packs.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Laraver\Waimai\Eleme\Packs;
+
+use Illuminate\Support\Collection;
+use Laraver\Waimai\Eleme\Core\Api;
+
+class Packs extends Api
+{
+    /**
+     * 查询店铺当前生效合同类型.
+     *
+     * @param $shopId
+     *
+     * @return Collection
+     */
+    public function getEffectServicePackContract($shopId)
+    {
+        $result = $this->parseJSON('eleme.packs.getEffectServicePackContract', ['shopId' => $shopId]);
+
+        return new Collection($result);
+    }
+}
